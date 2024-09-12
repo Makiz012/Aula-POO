7279b5493acc8f5e567c0f72ec5e58f214bb5596
Makiz012 <marcone.alves@escolar.ifrn.edu.br>
2024-09-12 16:29:15 -0300

    atividade 12/09/2024
    a

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿public class banco
+{
+
+    public string nome;
+
+    public int ndc;
+
+    public int CPF;
+
+    public int ddn;
+
+public banco(string nomedou , int numeroconta, int cpf, int DDN)
+{
+    nome = nomedou;
+    ndc = numeroconta;
+    CPF = cpf;
+    ddn = DDN;
+}
+
+
+}
---
+++ banco.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class principal{
+
+    static void Main (string[] args){
+
+        banco usuario1 = new banco ("Eduardo Oliveira" , 83638343, 473460350,15091995);
+    }
+}
